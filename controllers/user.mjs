835ba47f1ea3a983485688db3921131b9cf2a288
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-08-06 12:16:49 +0400

    Update user.mjs

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -40,6 +40,29 @@ export const signIn = async (req, res) => {
     }
 };
 
+export const signInAsAdmin = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        if (user.password !== password) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        if (!user.isAdmin) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Sign-in successful', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
 export const addToCard = async (req, res) => {
     const { id, product } = req.body;
 
